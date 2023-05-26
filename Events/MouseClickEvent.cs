b4136951c1a602bf9acf14f406bb2febf4c39e7f
leftbones <evanjparker@outlook.com>
2023-05-25 19:57:36 -0500

    Added MouseClickEvent.cs

---
+++ Events/MouseClickEvent.cs
@@ -0,0 +1,20 @@
+﻿using System.Numerics;
+using Raylib_cs;
+
+namespace Skeleton.Events;
+
+public class MouseLeftEvent : Event {
+    public Vector2 Position { get; private set; }
+
+    public MouseLeftEvent(Vector2 position) {
+        Position = position;
+    }
+}
+
+public class MouseRightEvent : Event {
+    public Vector2 Position { get; private set; }
+
+    public MouseRightEvent(Vector2 position) {
+        Position = position;
+    }
+}
